f33ec1af4c78417a44b7029691d4b73d47e31fb7
Jan Horava <czechinthemail@gmail.com>
2015-08-23 17:56:47 -0700

    moved disabled input css to css file

--- js/particleball-2d.js
+++ js/particleball-2d.js
@@ -588,8 +588,8 @@ function setmode(newmode, newgame){
     buffer_static = 0;
     canvas = 0;
 
-    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">AI vs AI</a><br><a onclick="setmode(2, true)">Player vs AI</a></div></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
-      + settings['movement-keys'] + '>Move ←→<br><input disabled style=border:0 value=Click>Obstacles++<br><input id=restart-key maxlength=1 value='
+    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">AI vs AI</a><br><a onclick="setmode(2, true)">Player vs AI</a></div></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
+      + settings['movement-keys'] + '>Move ←→<br><input disabled value=Click>Obstacles++<br><input id=restart-key maxlength=1 value='
       + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=score-goal value='
       + settings['score-goal'] + '>Goal<br>Level:<ul><li><input id=gamearea-height value='
